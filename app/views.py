2cf373d5b3e6b6b9d1028836252b9020c5b31360
nonewind <wn345361049@163.com>
2019-09-07 16:12:23 +0800

    modified:   app/__init__.py 	modified:   app/__pycache__/__init__.cpython-37.pyc 	modified:   app/__pycache__/views.cpython-37.pyc 	modified:   app/templates/base.html 	modified:   app/templates/index.html 	modified:   app/templates/rents.html 	modified:   app/views.py 	app/__pycache__/rents.cpython-37.pyc     app/static/img/ 	app/templates/About.html 	app/templates/help.html 	app/templates/joinin.html

--- app/views.py
+++ app/views.py
@@ -3,9 +3,35 @@ from flask import render_template
 
 @app.route('/')
 @app.route('/index')
-
 def index():
     return render_template(
-        'index.html',
+        'index.html'
+    )
+
+@app.route('/rents')
+def rents():
+    return render_template(
+        "rents.html",
         title = '快点回家'
+    )
+
+@app.route('/about')
+def about():
+    return render_template(
+        "About.html",
+        title = '嘿嘿嘿...其实我是...'
+    )
+
+@app.route('/help')
+def help():
+    return render_template(
+        'help.html',
+        title = '救救孩子吧...'
+    )
+
+@app.route('/joinin')
+def joinin():
+    return render_template(
+        'joinin.html',
+        title = '少年我看你骨骼精奇...'
     )
