3d1aa3f588d0a062cca14ff87236594e6914881c
nonewind <wn345361049@163.com>
2019-09-11 22:45:10 +0800

    modified:   app.db 	modified:   app/__pycache__/views.cpython-37.pyc modified:   app/static/css/style-GD.css 	modified:   app/static/css/style.css 	new file:   app/static/img/ZZ.png 	new file:   app/static/img/ZZ_1.png modified:   app/templates/base_GD.html 	modified:   app/templates/help.html 	modified:   app/templates/rents.html 	modified:   app/views.py

--- app/views.py
+++ app/views.py
@@ -1,5 +1,8 @@
 from app import app
 from flask import render_template,jsonify
+import sqlite3
+
+DATABASE = 'D:\CODE/flask_text/app.db'
 
 @app.route('/')
 @app.route('/index')
@@ -32,6 +35,26 @@ def help():
 @app.route('/rents')
 def rents():
     return render_template(
+        'rents.html'
+    )
+
+@app.route('/rents/<name>',methods=['GET'])
+def rents_test(name):
+    sqlite3DB = sqlite3.connect(DATABASE)
+    if name == '0':
+        cur = sqlite3DB.execute("select QingDao_00 from Location")
+        curss = cur.fetchall()
+    else:
+        return '数据库报错?????'
+
+    sqlite3DB.close()
+    curss_list = ''
+    for i in curss:
+        i = str(i)
+        i = i.replace("('","")
+        i = i.replace("',)",'')
+        curss_list = curss_list+i+','
+    return render_template(
         'rents.html',
-        title = '我在等你回家'
+        name = curss_list
     )
