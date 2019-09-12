832715327fe460021db8520373b7cb95120c64b8
nonewind <wn345361049@163.com>
2019-09-12 14:33:46 +0800

    modified:   SQL_wirte.py 	deleted:    app/__pycache__/__init__.cpython-37.pyc 	deleted:    app/__pycache__/models.cpython-37.pyc 	deleted:    app/__pycache__/views.cpython-37.pyc 	modified:   app/models.py 	modified:   app/templates/rents.html 	modified:   data_loc.db

--- app/models.py
+++ app/models.py
@@ -1,12 +1,11 @@
 from app import db
 
 class Qingdao_0(db.Model):
-
-    loc = db.Column(db.String(128),index = True,nullable=True,primary_key=True) #青岛0~500 元 demo
+    loc = db.Column(db.String(128),index = True,nullable=True,primary_key = True) #青岛0~500 元 demo
 class Qingdao_1(db.Model):
-    loc = db.Column(db.String(128),index = True,nullable=True,primary_key=True) #青岛0~500 元 demo
+    loc = db.Column(db.String(128),index = True,nullable=True,primary_key = True) #青岛0~500 元 demo
 class Qingdao_2(db.Model):
-    loc = db.Column(db.String(128),index = True,nullable=True,primary_key=True) #青岛0~500 元 demo
+    loc = db.Column(db.String(128),index = True,nullable=True,primary_key = True) #青岛0~500 元 demo
 
     def __repr__(self):
-        return '<Location %r >' % (self.QingDao_00)
+        return '<qingdao_0 %r >' % (self.loc)
