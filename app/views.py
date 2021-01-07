e8085c4b49c20864b096a5e09c705d7213280ddf
nonewind <wn345361049@163.com>
2021-01-07 17:25:42 +0800

    modified:   app/templates/rents.html 	modified:   app/views.py

--- app/views.py
+++ app/views.py
@@ -28,33 +28,10 @@ def more():
     )
 
 
-@app.route('/rents/<name>', methods=['GET'])
-def rents_test(name):
-    sqlite3DB = sqlite3.connect(DATABASE)
-    if name == '0':
-        cur = sqlite3DB.execute("select * from QingDao_0")
-        curss = cur.fetchall()
-    elif name == '1':
-        cur = sqlite3DB.execute("select * from QingDao_1")
-        curss = cur.fetchall()
-    elif name == '2':
-        cur = sqlite3DB.execute("select * from QingDao_2")
-        curss = cur.fetchall()
-    else:
-        return "The Page 404 ! "
-    page_number = int(name) + 2
-    sqlite3DB.close()
-    curss_list = ''
-    for i in curss:
-        i = str(i)
-        i = i.replace("('", "")
-        i = i.replace("',)", '')
-        curss_list = curss_list+i+','
-    return render_template(
-        'rents.html',
-        name=curss_list,
-        page=page_number
-    )
+@app.route("/rentsData",methods=['POST'])
+def rentsData():
+    return('{"xx":"台柳路附近，照片实图精装朝南主卧独卫特价出租免中介  "}')
+
 
 @app.route('/getCityList',methods=['GET'])
 def getCityList():
