9eaed2e7fc3c6e8688e17a319e98157ac3598261
ZihenZhang <wn345361049@gmail.com>
2021-03-16 10:14:05 +0800

    modified:   app/templates/rents.html 	renamed:    spider/cityAppnd.py -> cityAppnd.py 	modified:   config.py 	new file:   spider-58.py 	deleted:    spider/README.md 	deleted:    spider/config.py 	deleted:    spider/spider-58.py

--- config.py
+++ config.py
@@ -1,3 +1,8 @@
-MONGO = 'mongodb://127.0.0.1:27017'
+'''
+Author: Ziheng
+Date: 2021-01-11 10:19:28
+LastEditTime: 2021-03-16 09:12:56
+'''
+MONGO = 'mongodb://149.129.121.154:29999'
 DB = "Fuck_58"
 USER = "spider_user"
